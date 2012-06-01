aa2ba475efab7ee8f1855cf8f700a2937b6e16c9
julio <jcrinconc@gmail.com>
2012-06-01 15:09:45 +1000

    updated package names and maven group id

---
+++ src/main/resources/com/github/julior/springinspector/builtin-functions.js
@@ -0,0 +1,66 @@
+importPackage(java.util);
+importPackage(java.lang);
+importPackage(java.io);
+
+
+//converts a JS object into a java HashMap
+function toMap(obj){
+    var hm = new HashMap()
+    for(k in obj){
+        hm.put(k, toJava(obj[k]));
+    }
+    return hm;
+}
+
+//convert a JS array into a java ArrayList
+
+function toArray(arr){
+    var list = new ArrayList(arr.length);
+    for(i in arr)
+        list.add(toJava(arr[i]));
+    return list;
+}
+
+//converts a json object into the equivalent java object
+function toJava(obj){
+    if(obj instanceof Array){
+        return toArray(obj);
+    } else if(obj instanceof java.lang.Object){
+        return obj;
+    } else if(typeof obj == "object"){
+        return toMap(obj);
+    } else{
+        return obj;
+    }
+}
+//add more functions
+
+function getField(obj, fld){
+    var field = obj.getClass().getDeclaredField(fld);
+    field.setAccessible(true);
+    return field.get(obj);
+}
+
+//exec a process
+function exec(cmd){
+    var cmdarr = (cmd instanceof Array)?cmd:cmd.split(/\s+/)
+    var proc = new ProcessBuilder(cmdarr).redirectErrorStream(true).start();
+    var reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+    var line = null;
+    var resp = new StringBuilder();
+    var sep = System.getProperty("line.separator");
+    while((line=reader.readLine())!=null){
+        resp.append(line).append(sep);
+    }
+    proc.waitFor();
+    proc.exitValue();
+    return resp.toString();
+}
+
+//register functions below
+
+var functions = toJava({"toJava":toJava, "getField":getField, "exec":exec});
+
+//return
+functions
+
